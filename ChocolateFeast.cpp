785126634279ce520fcbe8ee251d7a649e25b95d
Simranjeet Singh <simranjeet.singh8495@gmail.com>
2016-02-11 17:25:57 +0530

    11-Feb-2016  https://www.hackerrank.com/challenges/chocolate-feast

---
+++ ChocolateFeast.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    long N,C,M;
+    
+    int dn,total=0;
+    cin>>T;
+    while(T--){
+        cin>>N>>C>>M;
+        
+        dn = N/C;
+        total = dn;
+        
+        while(dn){
+            if((dn-M)>=0){
+                dn = dn-M;
+                total++;
+                dn++;
+            }
+        }
+        cout<<total<<"\n";
+    }
+}
